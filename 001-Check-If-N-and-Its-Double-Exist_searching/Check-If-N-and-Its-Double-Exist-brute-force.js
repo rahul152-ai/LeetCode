5d1173ec1108f9688c1e09130d55c7c1824a1182
dbrahul <dbrahul03@gmail.com>
2025-05-11 11:34:36 +0530

    if n and its double exist in array brute force approach

---
+++ 001-Check-If-N-and-Its-Double-Exist_searching/Check-If-N-and-Its-Double-Exist-brute-force.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+
+var checkifExist = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] === 2 * arr[j] && i != j) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
