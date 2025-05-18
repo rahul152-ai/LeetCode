2e83f95d90848389edc7910e689747baa02feda4
= <=>
2025-05-18 11:22:11 +0530

    move all non-zeros to left and zeroes to right

--- 004-Move-Zeroes/001-move-zero-to-end.js
+++ 004-Move-Zeroes/001-move-zero-to-end.js
@@ -2,6 +2,9 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+
+// The function moves all zeroes in the array to the end while maintaining the order of non-zero elements.
+// It uses a two-pointer approach to achieve this in O(n) time complexity and O(1) space complexity.
 var moveZeroes = function (nums) {
   let count = 0;
   for (let i = 0; i < nums.length; i++) {
